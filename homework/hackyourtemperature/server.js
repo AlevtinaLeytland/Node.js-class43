a6cf51eddc45a9bb5f6ab7c4e450c3023ac128bb
AlevtinaLeytland <clavusclavo321@gmail.com>
2023-06-04 20:35:25 +0200

    separate  week1 changes

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,4 +1,8 @@
 import fetch from "node-fetch";
+const app = express();
+app.use(express(json))
+
+// const API_KEY = "3ea96a8f2c2e6006050fa5167a9e20e8";
 
 const path = {
   root: "/",
@@ -6,13 +10,14 @@ const path = {
 };
 
 app.get(path.root, (req, res) => {
+  
   res.send("hello from backend to frontend");
 });
 app.listen(3000, () => {
   console.log("Server started");
 });
 
-app.post(path.weather, async (req, res) => {
+app.post(path.weather,(req, res) => {
   const cityName = req.body.cityName;
-  res.send({ weatherText: `${cityName}` });
+  res.send({`City Name is ${cityName}` });
 });
